ead350388b4a5ae04a4310b284658d5d413dd278
tanvirch0750 <tanvirch7575@gmail.com>
2024-10-12 03:02:30 +0600

    fixed the type bug

--- app/(main)/_components/product/AvailableDetailsAdmin.tsx
+++ app/(main)/_components/product/AvailableDetailsAdmin.tsx
@@ -1,35 +1,35 @@
-'use client';
+// 'use client';
 
-import Link from 'next/link';
-import { ColorPicker } from './ColorPicker';
-import { SizePicker } from './SizePicker';
-import { useState } from 'react';
+// import Link from 'next/link';
+// import { ColorPicker } from './ColorPicker';
+// import { SizePicker } from './SizePicker';
+// import { useState } from 'react';
 
-export default function AvailableDeatilsAdmin({ product }: { product: any }) {
-  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
-  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+// export default function AvailableDeatilsAdmin({ product }: { product: any }) {
+//   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
+//   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
 
-  return (
-    <>
-      <ColorPicker
-        colors={product?.colors}
-        selectedColor={selectedColor}
-        setSelectedColor={setSelectedColor}
-      />
-      <SizePicker
-        sizes={product?.sizes}
-        selectedSize={selectedSize}
-        setSelectedSize={setSelectedSize}
-      />
+//   return (
+//     <>
+//       <ColorPicker
+//         colors={product?.colors}
+//         selectedColor={selectedColor}
+//         setSelectedColor={setSelectedColor}
+//       />
+//       <SizePicker
+//         sizes={product?.sizes}
+//         selectedSize={selectedSize}
+//         setSelectedSize={setSelectedSize}
+//       />
 
-      <Link
-        href="/dashboard/edit/id"
-        className={`mt-8 flex w-full items-center justify-center rounded-md 
-                  
-                  px-8 py-3 text-base font-medium text-white bg-brand hover:bg-brand `}
-      >
-        Edit
-      </Link>
-    </>
-  );
-}
+//       <Link
+//         href="/dashboard/edit/id"
+//         className={`mt-8 flex w-full items-center justify-center rounded-md
+
+//                   px-8 py-3 text-base font-medium text-white bg-brand hover:bg-brand `}
+//       >
+//         Edit
+//       </Link>
+//     </>
+//   );
+// }
